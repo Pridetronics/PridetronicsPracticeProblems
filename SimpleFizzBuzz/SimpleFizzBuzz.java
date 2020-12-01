9ad5668ec83e5d88d290286bb7b55d86a32c2969
mzakocs <mzakocs@gmail.com>
2020-12-01 13:41:08 -0700

    Updated solution to fizzbuzz

--- SimpleFizzBuzz/SimpleFizzBuzz.java
+++ SimpleFizzBuzz/SimpleFizzBuzz.java
@@ -1,21 +1,35 @@
 import java.util.Scanner;
+
 class SimpleFizzBuzz {
     public static void main(String[] args) {
-        //Initiates scanner object
+
+        System.out.println("Pick a number between one and one hundred.");
+
+        //creates scanner object
         Scanner scanner = new Scanner(System.in);
-        //Ask the user for the number
-        System.out.println("Enter a number between 1-100");
-        double number = scanner.nextDouble();
+
+        //defines the user input as "number" and defines it as an integer
+        int number = scanner.nextInt();
+
+        //prints the users input
         System.out.println(number);
-        
-        if (number % 3 == 0) {
-            System.out.println("Fizz");
-        }
-        if (number % 5 == 0) {
-            System.out.println("Buzz");
-        }
-        if (number % 5 == 0 && number % 3 == 0){
+
+        //first if statement made in order to determine if the number is both divisible by 3 and 5 to print FizzBuzz
+        if (number % 3 == 0 && number % 5 == 0) {
             System.out.println("FizzBuzz");
         }
-    }  
-} 
+
+        //then i used an else if statement so that it could be if and ONLY if the number is divisible by 3 to print Fizz
+        else if (number % 3 == 0) {
+            System.out.println("Fizz");
+        }
+
+        //then i used a 2nd else if statement so that it could be if and ONLY if the number is divisible by 5 to print Buzz    
+        else if (number % 5 == 0) {
+            System.out.println("Buzz");
+        }
+
+        //closed the scanner object
+        scanner.close();
+    }
+}
